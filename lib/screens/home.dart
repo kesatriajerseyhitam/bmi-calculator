7c6ff805bc826968827545e6fc4cfc164e204163
Raka <mgf.prauliyatama@gmail.com>
2020-03-27 21:27:25 +0700

    feature(home): add calculate button:

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../widgets/custom_card.dart';
 
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+const bottomContainerHeight = 70.0;
+const bottomContainerTextSize = 25.0;
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -21,12 +26,12 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: CustomCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColour,
                   ),
                 ),
                 Expanded(
                   child: CustomCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColour,
                   ),
                 ),
               ],
@@ -34,7 +39,7 @@ class _HomeState extends State<Home> {
           ),
           Expanded(
             child: CustomCard(
-              colour: Color(0xFF1D1E33),
+              colour: activeCardColour,
             ),
           ),
           Expanded(
@@ -42,15 +47,29 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: CustomCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColour,
                   ),
                 ),
                 Expanded(
                   child: CustomCard(
-                    colour: Color(0xFF1D1E33),
+                    colour: activeCardColour,
                   ),
                 ),
               ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            height: bottomContainerHeight,
+            margin: EdgeInsets.only(top: 20),
+            width: double.infinity,
+            child: Center(
+              child: Text(
+                'calculate'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: bottomContainerTextSize,
+                ),
+              ),
             ),
           ),
         ],
